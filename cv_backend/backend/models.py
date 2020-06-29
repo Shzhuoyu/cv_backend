60ebb6b8a6f6f3005f1466ff586014a45ea399f1
owntime <188006886@qq.com>
2020-06-29 16:09:10 +0800

    完整数据库（待审查）

--- cv_backend/backend/models.py
+++ cv_backend/backend/models.py
@@ -15,11 +15,120 @@ class oldperson_info(models.Model):
     checkin_date = models.DateTimeField(),
     checkout_data = models.DateTimeField(),
     imgset_dir = models.CharField(max_length=200),
-    profile_photo = models.CharField(max_length=200)
+    profile_photo = models.CharField(max_length=200),
+    room_number = models.CharField(max_length=50),
+    firstguardian_name = models.CharField(max_length=50),
+    firstguardian_relationship = models.CharField(max_length=50),
+    firstguardian_phone = models.CharField(max_length=50),
+    firstguardian_wechat = models.CharField(max_length=50),
+
+    sceondguardian_name = models.CharField(max_length=50),
+    secondguardian_relationship = models.CharField(max_length=50),
+    secondguardian_phone = models.CharField(max_length=50),
+    secondguardian_wechat = models.CharField(max_length=50),
+    health_state = models.CharField(max_length=50),
+    DESCRIPTION = models.CharField(max_length=50),
+    ISACTIVE = models.CharField(max_length=10),
+    CREATED = models.DateTimeField(),
+    CREATEBY = models.IntegerField(max_length=11),
+    UPDATED = models.DateTimeField(),
+    UPDATEBY = models.IntegerField(max_length=11),
+    REMOVE = models.CharField(max_length=1),
+
 
     def __str__(self):
         return self.username
 
 
 class employee_info(models.Model):
-    id = models.IntegerField(max_length=11, primary_key=True)
+    id = models.IntegerField(max_length=11, primary_key=True),
+    ORG_ID = models.IntegerField(max_length=11),
+    CLIENT_ID = models.IntegerField(max_length=11),
+    username = models.CharField(max_length=50),
+    gender = models.CharField(max_length=5),  # f/m
+    phone = models.CharField(max_length=50),
+    id_card = models.CharField(max_length=50),  # 身份证
+    birthday = models.DateTimeField(),
+    hire_date = models.DateTimeField(),
+    resign_date = models.DateTimeField(),
+    imgset_dir = models.CharField(max_length=200),
+    profile_photo = models.CharField(max_length=200),
+    DESCRIPTION = models.CharField(max_length=200),
+    ISACTIVE = models.CharField(max_length=10),
+    CREATED = models.DateTimeField(),
+    CREATEBY = models.IntegerField(max_length=11),
+    UPDATED = models.DateTimeField(),
+    UPDATEBY = models.IntegerField(max_length=11),
+    REMOVE = models.CharField(max_length=1),
+
+    def __str__(self):
+        return self.username
+
+
+class volunteer_info(models.Model):
+    id = models.IntegerField(max_length=11, primary_key=True),
+    ORG_ID = models.IntegerField(max_length=11),
+    CLIENT_ID = models.IntegerField(max_length=11),
+    name = models.CharField(max_length=50),
+    gender = models.CharField(max_length=5),  # f/m
+    phone = models.CharField(max_length=50),
+    id_card = models.CharField(max_length=50),  # 身份证
+    birthday = models.DateTimeField(),
+    checkin_date = models.DateTimeField(),
+    checkout_date = models.DateTimeField(),
+    imgset_dir = models.CharField(max_length=200),
+    profile_photo = models.CharField(max_length=200),
+    DESCRIPTION = models.CharField(max_length=200),
+    ISACTIVE = models.CharField(max_length=10),
+    CREATED = models.DateTimeField(),
+    CREATEBY = models.IntegerField(max_length=11),
+    UPDATED = models.DateTimeField(),
+    UPDATEBY = models.IntegerField(max_length=11),
+    REMOVE = models.CharField(max_length=1),
+
+    def __str__(self):
+        return self.name
+
+
+class event_info(models.Model):
+    id = models.IntegerField(max_length=11,primary_key=True),
+    event_type = models.IntegerField(max_length=11),
+    event_date = models.DateTimeField(),
+    event_location = models.CharField(max_length=200),
+    event_desc = models.CharField(max_length=200),
+    oldperson_id = models.IntegerField(max_length=11),
+    img_path = models.CharField(max_length=200),
+
+    def __str__(self):
+        return self.event_desc
+
+
+class sys_user(models.Model):
+    id = models.IntegerField(max_length=11,primary_key=True),
+    ORG_ID = models.IntegerField(max_length=11),
+    CLIENT_ID = models.IntegerField(max_length=11),
+    UserName = models.CharField(max_length=50),
+    Password = models.CharField(max_length=50),
+    REAL_NAME = models.CharField(max_length=50),
+    SEX = models.CharField(max_length=20),
+    EMAIL = models.CharField(max_length=50),
+    PHONE = models.CharField(max_length=50),
+    MOBILE = models.CharField(max_length=50),
+    DESCRIPTION = models.CharField(max_length=200),
+    ISACTIVE = models.CharField(max_length=10),
+    CREATED = models.DateTimeField(),
+    CREATEBY = models.IntegerField(max_length=11),
+    UPDATED = models.DateTimeField(),
+    UPDATEBY = models.IntegerField(max_length=11),
+    REMOVE = models.CharField(max_length=1),
+    DATAFILTER = models.CharField(max_length=200),
+    theme = models.CharField(max_length=45),
+    defaultpage = models.CharField(max_length=45),
+    logoimage = models.CharField(max_length=45),
+    qqopenid = models.CharField(max_length=100),
+    appversion = models.CharField(max_length=10),
+    jsonauth = models.CharField(max_length=1000),
+
+    def __str__(self):
+        return self.UserName
+
--- cv_backend/backend/serializer.py
+++ cv_backend/backend/serializer.py
@@ -5,10 +5,36 @@ from .models import *
 class OldPersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = oldperson_info
-        fields = ('id', 'org_id', 'client_id', 'username')  # TODO
+        fields = ('id', 'org_id', 'client_id', 'username', 'gender', 'phone', 'id_card', 'birthday', 'checkin_date', 'checkout_date', 'imgset_dir', 'profile_photo', 'room_number',
+                  'firstguardian_name', 'firstguardian_relationship', 'firstguardian_phone', 'firstguardian_wechat',
+                  'secondguardian_name', 'secondguardian_relationship', 'secondguardian_phone', 'secondguardian_wechat',
+                  'health_state', 'description', 'isactive', 'created', 'createby', 'updated', 'updateby', 'remove')  # TODO
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = employee_info
-        fields = ('id', 'org_id', 'client_id', 'username')  # TODO
+        fields = ('id', 'org_id', 'client_id', 'username', 'gender', 'phone', 'id_card', 'birthday',
+                  'hire_date', 'resign_date', 'imgset_dir', 'profile_photo', 'description', 'isactive', 'created', 'createby', 'updated', 'updateby', 'remove')  # TODO
+
+
+class VolunteerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = volunteer_info
+        fields = ('id', 'org_id', 'client_id', 'name', 'gender', 'phone', 'id_card', 'birthday',
+                  'checkin_date', 'checkout_date', 'imgset_dir', 'profile_photo', 'description', 'isactive',
+                  'created', 'createby', 'updated', 'updateby', 'remove')  # TODO
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = event_info
+        fields = ('id', 'event_type', 'event_date', 'event_location', 'event_desc', 'oldperson_id', 'img_path')
+
+
+class SysUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = sys_user
+        fields = ('id', 'org_id', 'client_id', 'username', 'password', 'real_name', 'sex', 'email', 'phone', 'mobile',
+                  'description', 'isactive', 'created', 'createby', 'updated', 'updateby', 'remove', 'datafilter',
+                  'theme', 'defaultpage', 'logoimage', 'qqopenid', 'appversion', 'jsonauth')
