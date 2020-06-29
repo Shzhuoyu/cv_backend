bc75d587cb7e2617acfc95cfca3520e2fdab8398
shzhuoyu <971993723@qq.com>
2020-06-29 20:26:45 +0800

    数据库

--- cv_backend/backend/models.py
+++ cv_backend/backend/models.py
@@ -32,7 +32,7 @@ class oldperson_info(models.Model):
     CREATEBY = models.IntegerField(blank=True)
     UPDATED = models.DateTimeField(blank=True)
     UPDATEBY = models.IntegerField(blank=True)
-    REMOVE = models.CharField(max_length=1,blank=True)
+    REMOVE = models.CharField(max_length=1, blank=True)
 
 
     def __str__(self):
@@ -41,24 +41,24 @@ class oldperson_info(models.Model):
 
 class employee_info(models.Model):
     id = models.IntegerField(primary_key=True)
-    ORG_ID = models.IntegerField()
-    CLIENT_ID = models.IntegerField()
-    username = models.CharField(max_length=50)
-    gender = models.CharField(max_length=5)  # f/m
-    phone = models.CharField(max_length=50)
-    id_card = models.CharField(max_length=50)  # 身份证
+    ORG_ID = models.IntegerField(blank=True)
+    CLIENT_ID = models.IntegerField(blank=True)
+    username = models.CharField(max_length=50, blank=True)
+    gender = models.CharField(max_length=5, blank=True)  # f/m
+    phone = models.CharField(max_length=50, blank=True)
+    id_card = models.CharField(max_length=50, blank=True)  # 身份证
     birthday = models.DateTimeField()
     hire_date = models.DateTimeField()
     resign_date = models.DateTimeField()
-    imgset_dir = models.CharField(max_length=200)
-    profile_photo = models.CharField(max_length=200)
-    DESCRIPTION = models.CharField(max_length=200)
-    ISACTIVE = models.CharField(max_length=10)
+    imgset_dir = models.CharField(max_length=200, blank=True)
+    profile_photo = models.CharField(max_length=200, blank=True)
+    DESCRIPTION = models.CharField(max_length=200, blank=True)
+    ISACTIVE = models.CharField(max_length=10, blank=True)
     CREATED = models.DateTimeField()
-    CREATEBY = models.IntegerField()
+    CREATEBY = models.IntegerField(blank=True)
     UPDATED = models.DateTimeField()
-    UPDATEBY = models.IntegerField()
-    REMOVE = models.CharField(max_length=1)
+    UPDATEBY = models.IntegerField(blank=True)
+    REMOVE = models.CharField(max_length=1, blank=True)
 
     def __str__(self):
         return self.username
@@ -66,24 +66,24 @@ class employee_info(models.Model):
 
 class volunteer_info(models.Model):
     id = models.IntegerField(primary_key=True)
-    ORG_ID = models.IntegerField()
-    CLIENT_ID = models.IntegerField()
-    name = models.CharField(max_length=50)
-    gender = models.CharField(max_length=5) # f/m
-    phone = models.CharField(max_length=50)
-    id_card = models.CharField(max_length=50) # 身份证
+    ORG_ID = models.IntegerField(blank=True)
+    CLIENT_ID = models.IntegerField(blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    gender = models.CharField(max_length=5, blank=True) # f/m
+    phone = models.CharField(max_length=50, blank=True)
+    id_card = models.CharField(max_length=50, blank=True) # 身份证
     birthday = models.DateTimeField()
     checkin_date = models.DateTimeField()
     checkout_date = models.DateTimeField()
-    imgset_dir = models.CharField(max_length=200)
-    profile_photo = models.CharField(max_length=200)
-    DESCRIPTION = models.CharField(max_length=200)
-    ISACTIVE = models.CharField(max_length=10)
+    imgset_dir = models.CharField(max_length=200, blank=True)
+    profile_photo = models.CharField(max_length=200, blank=True)
+    DESCRIPTION = models.CharField(max_length=200, blank=True)
+    ISACTIVE = models.CharField(max_length=10, blank=True)
     CREATED = models.DateTimeField()
-    CREATEBY = models.IntegerField()
+    CREATEBY = models.IntegerField(blank=True)
     UPDATED = models.DateTimeField()
-    UPDATEBY = models.IntegerField()
-    REMOVE = models.CharField(max_length=1)
+    UPDATEBY = models.IntegerField(blank=True)
+    REMOVE = models.CharField(max_length=1, blank=True)
 
     def __str__(self):
         return self.name
@@ -91,12 +91,12 @@ class volunteer_info(models.Model):
 
 class event_info(models.Model):
     id = models.IntegerField(primary_key=True)
-    event_type = models.IntegerField()
+    event_type = models.IntegerField(blank=True)
     event_date = models.DateTimeField()
-    event_location = models.CharField(max_length=200)
-    event_desc = models.CharField(max_length=200)
-    oldperson_id = models.IntegerField()
-    img_path = models.CharField(max_length=200)
+    event_location = models.CharField(max_length=200, blank=True)
+    event_desc = models.CharField(max_length=200, blank=True)
+    oldperson_id = models.IntegerField(blank=True)
+    img_path = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.event_desc
@@ -104,29 +104,29 @@ class event_info(models.Model):
 
 class sys_user(models.Model):
     id = models.IntegerField(primary_key=True)
-    ORG_ID = models.IntegerField()
-    CLIENT_ID = models.IntegerField()
-    UserName = models.CharField(max_length=50)
-    Password = models.CharField(max_length=50)
-    REAL_NAME = models.CharField(max_length=50)
-    SEX = models.CharField(max_length=20)
-    EMAIL = models.CharField(max_length=50)
-    PHONE = models.CharField(max_length=50)
-    MOBILE = models.CharField(max_length=50)
-    DESCRIPTION = models.CharField(max_length=200)
-    ISACTIVE = models.CharField(max_length=10)
+    ORG_ID = models.IntegerField(blank=True)
+    CLIENT_ID = models.IntegerField(blank=True)
+    UserName = models.CharField(max_length=50, blank=True)
+    Password = models.CharField(max_length=50, blank=True)
+    REAL_NAME = models.CharField(max_length=50, blank=True)
+    SEX = models.CharField(max_length=20, blank=True)
+    EMAIL = models.CharField(max_length=50, blank=True)
+    PHONE = models.CharField(max_length=50, blank=True)
+    MOBILE = models.CharField(max_length=50, blank=True)
+    DESCRIPTION = models.CharField(max_length=200, blank=True)
+    ISACTIVE = models.CharField(max_length=10, blank=True)
     CREATED = models.DateTimeField()
-    CREATEBY = models.IntegerField()
+    CREATEBY = models.IntegerField(blank=True)
     UPDATED = models.DateTimeField()
-    UPDATEBY = models.IntegerField()
-    REMOVE = models.CharField(max_length=1)
-    DATAFILTER = models.CharField(max_length=200)
-    theme = models.CharField(max_length=45)
-    defaultpage = models.CharField(max_length=45)
-    logoimage = models.CharField(max_length=45)
-    qqopenid = models.CharField(max_length=100)
-    appversion = models.CharField(max_length=10)
-    jsonauth = models.CharField(max_length=1000)
+    UPDATEBY = models.IntegerField(blank=True)
+    REMOVE = models.CharField(max_length=1, blank=True)
+    DATAFILTER = models.CharField(max_length=200, blank=True)
+    theme = models.CharField(max_length=45, blank=True)
+    defaultpage = models.CharField(max_length=45, blank=True)
+    logoimage = models.CharField(max_length=45, blank=True)
+    qqopenid = models.CharField(max_length=100, blank=True)
+    appversion = models.CharField(max_length=10, blank=True)
+    jsonauth = models.CharField(max_length=1000, blank=True)
 
     def __str__(self):
         return self.UserName
